a92d91be53967142d03f008d9dd69dd197ce86a6
Cooper Chen <cooper.chen@titansoft.com.sg>
2019-01-13 15:18:27 +0700

    Given an array of integers, return indices of the two numbers such that they add up to a specific target.
    You may assume that each input would have exactly one solution, and you may not use the same element twice.
    
    Example:
    
    Given nums = [2, 7, 11, 15], target = 9,
    
    Because nums[0] + nums[1] = 2 + 7 = 9,
    return [0, 1].

---
+++ TwoSum/leetcode001.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoSum
+{
+    public class Leetcode001
+    {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            int[] returnValue = null;
+            if(nums==null)
+                return returnValue;
+
+            if (nums.Length < 2)
+                return returnValue;
+
+            for (int i = 0; i < nums.Length; i++)
+                {
+                    if (nums[i] < target)
+                    {
+                        for (int j = 0; j < nums.Length; j++)
+                        {
+                            if (j != i && nums[j] < target)
+                            {
+                                if (nums[i] + nums[j] == target)
+                                {
+                                    returnValue = new int[] { i, j };
+                                    return returnValue;
+                                }
+                            }
+                        }
+                    }
+                }
+            return returnValue;
+        }
+    }
+}
---
+++ TwoSumTests/Leetcode001Tests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TwoSum;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoSum.Tests
+{
+    [TestClass()]
+    public class Leetcode001Tests
+    {
+        [TestMethod()]
+        public void TwoSumTest()
+        {
+            int[] expectedValue;
+            int[] inputValue;
+            int[] resultValue;
+
+            expectedValue = new int[] { 0, 1 };
+            inputValue = new int[] { 2, 7, 11, 15 };
+            resultValue = Leetcode001.TwoSum(inputValue, 9);
+            Assert.IsTrue(expectedValue[0] == resultValue[0] && expectedValue[1] == resultValue[1]);
+
+            expectedValue = new int[] { 1, 2 };
+            inputValue = new int[] { 1, 7, 2, 15 };
+            resultValue = Leetcode001.TwoSum(inputValue, 9);
+            Assert.IsTrue(expectedValue[0] == resultValue[0] && expectedValue[1] == resultValue[1]);
+
+            expectedValue = new int[] { 0, 4 };
+            inputValue = new int[] { 2, 17, 11, 15, 7 };
+            resultValue = Leetcode001.TwoSum(inputValue, 9);
+            Assert.IsTrue(expectedValue[0] == resultValue[0] && expectedValue[1] == resultValue[1]);
+
+            expectedValue = null;
+            inputValue = null;
+            resultValue = Leetcode001.TwoSum(inputValue, 9);
+            Assert.IsTrue(expectedValue == resultValue);
+
+            expectedValue = null;
+            inputValue = new int[] { 0 };
+            resultValue = Leetcode001.TwoSum(inputValue, 9);
+            Assert.IsTrue(expectedValue == resultValue);
+
+            //Assert.AreEqual(expectedValue, Leetcode001.TwoSum(inputValue, 9));
+        }
+    }
+}
